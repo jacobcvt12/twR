06157e4b134a2a85dee48c750ff58c5ff128ff1c
Jacob Carey <jacobcvt12@gmail.com>
2015-11-30 15:19:13 -0500

    Export distribution functions

--- R/distributions.R
+++ R/distributions.R
@@ -1,15 +1,19 @@
+#' @export
 dtw <- function(x, beta=1, log=FALSE) {
     return(x)
 }
 
+#' @export
 ptw <- function(q, beta=1, lower.tail=TRUE, log.p=FALSE) {
     return(q)
 }
 
+#' @export
 qtw <- function(p, beta=1, lower.tail=TRUE, log.p=FALSE) {
     return(p)
 }
 
+#' @export
 rtw <- function(n, beta=1) {
     return(rep(1, n))
 }
