0bbf8418beb3c89ae7e6a1c0775c171102198fe1
Jacob Carey <jacobcvt12@gmail.com>
2015-12-01 21:18:24 -0500

    Write differential equations for TW dist's
    Not quite working yet

--- R/painleve.R
+++ R/painleve.R
@@ -1,2 +1,20 @@
 painleve <- function() {
 }
+
+deq <- function(t, y, parms) {
+    list(c(y[2],
+           t * y[1] + 2 * y[1] ^ 3,
+           y[4],
+           y[1] ^ 2,
+           y[1]))
+}
+
+t0 <- 5; tn <- -8
+tspan <- seq(t0, tn, length.out=1000)
+y0 <- c(airy_Ai(t0),
+        airy_Ai_deriv(t0),
+        0,
+        airy_Ai(t0) ^ 2,
+        0)
+
+F.ode <- ode(y=y0, times=tspan, func=deq, parms=NULL, method="ode45")
--- R/twr.R
+++ R/twr.R
@@ -3,5 +3,5 @@
 #' @docType package
 #' @name twr
 #' @importFrom gsl airy_Ai airy_Ai_deriv
-#' @importFrom bvpSolve bvptwp
+#' @importFrom deSolve ode
 NULL
